a0ff972c2981f886d7e662e887bd8be278258972
danro9685 <daniele.ramazzotti@yahoo.com>
2016-07-08 11:45:44 -0700

    fix the variables numbers

--- final_code/experiments_final/experiments.consensus.poset.R
+++ final_code/experiments_final/experiments.consensus.poset.R
@@ -24,29 +24,31 @@ for (runs in names(results_bootstrap[["run"]])) {
     cont = cont + 1
     cat(cont/length(names(results_bootstrap[["run"]])),"\n")
     for (var in names(results_bootstrap[["run"]][[runs]])) {
-        for (den in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]])) {
-            curr_ground_true = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["adj.matrix"]][["adj.matrix.true"]]
-            #for (den in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]])) {
-                for (samples in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]])) {
-                    for (noise_levels in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]][[samples]])) {
-                        for (reg in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]])) {
-                            my.curr.reg = gsub(".hc","",gsub("adj.matrix.","",reg))
-                            if(var=="continuous") {
-                                my.curr.reg = paste0(my.curr.reg,"-g")
+        for (n_s in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]])) {
+            for (den in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]])) {
+                curr_ground_true = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["adj.matrix"]][["adj.matrix.true"]]
+                #for (den in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]])) {
+                    for (samples in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]])) {
+                        for (noise_levels in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]][[samples]])) {
+                            for (reg in names(results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]])) {
+                                my.curr.reg = gsub(".hc","",gsub("adj.matrix.","",reg))
+                                if(var=="continuous") {
+                                    my.curr.reg = paste0(my.curr.reg,"-g")
+                                }
+                                curr_dataset = read.table(file=paste0(getwd(),"/data/run/",runs,"/",var,"/","node_size/",as.character(n_s),"/density/",den,"/dataset_sample_size_",samples,"_noise_level_",noise_levels,".txt"),header=FALSE,sep=",",check.names=FALSE,stringsAsFactors=FALSE)
+                                curr_bootstrap_results = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]][["boot"]]
+                                curr_cardinality = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]][["card"]]
+                                curr_consensus_estimation = perform.consensus.likelihood.fit(curr_bootstrap_results,curr_dataset,my.curr.reg)
+                                curr_stats = getStats(curr_ground_true,curr_consensus_estimation)
+                                # save the results
+                                results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["adj.matrix"]][["adj.matrix.true"]] = curr_ground_true
+                                results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]] = curr_consensus_estimation
+                                results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][[n_s]][["density"]][[den]][["stats"]][[samples]][[noise_levels]][[reg]] = curr_stats
                             }
-                            curr_dataset = read.table(file=paste0(getwd(),"/data/run/",runs,"/",var,"/","node_size/10/density/",den,"/dataset_sample_size_",samples,"_noise_level_",noise_levels,".txt"),header=FALSE,sep=",",check.names=FALSE,stringsAsFactors=FALSE)
-                            curr_bootstrap_results = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]][["boot"]]
-                            curr_cardinality = results_bootstrap[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]][["card"]]
-                            curr_consensus_estimation = perform.consensus.likelihood.fit(curr_bootstrap_results,curr_dataset,my.curr.reg)
-                            curr_stats = getStats(curr_ground_true,curr_consensus_estimation)
-                            # save the results
-                            results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["adj.matrix"]][["adj.matrix.true"]] = curr_ground_true
-                            results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["reconstructions"]][[samples]][[noise_levels]][[reg]] = curr_consensus_estimation
-                            results_consensus_poset[["run"]][[runs]][[var]][["node_size"]][["10"]][["density"]][[den]][["stats"]][[samples]][[noise_levels]][[reg]] = curr_stats
                         }
                     }
-                }
-            #}
+                #}
+            }
         }
     }
 }
