cdc4286bf61ae2f2f17f78e649dc36b82fb7c183
Giulio Caravagna <caravagn@di.unipi.it>
2016-07-14 01:09:40 +0100

    puccettone

--- statistics/guido.plot.R
+++ statistics/guido.plot.R
@@ -1,11 +1,11 @@
 
-guido.plot = function(true.model, consensus.model, X.scores, Y.scores, labels = c('NPB','MI'), loop = NULL)
+guido.plot = function(true.model, consensus.model, standard.model, X.scores, Y.scores, labels = c('NPB','MI'), loop = NULL)
 {
 	if(!is.matrix(true.model)) true.model = as.matrix(true_adj_matrix)
 	if(!is.matrix(X.scores)) X.scores = as.matrix(X.scores)
 	if(!is.matrix(Y.scores)) Y.scores = as.matrix(Y.scores)
 	if(!is.matrix(consensus.model)) consensus.model = as.matrix(consensus.model)
-	# if(!is.matrix(aic.model)) aic.model = as.matrix(aic.model)
+	if(!is.matrix(standard.model)) standard.model = as.matrix(standard.model)
 	
 	ordering = order(X.scores)
 
@@ -26,7 +26,7 @@ guido.plot = function(true.model, consensus.model, X.scores, Y.scores, labels = 
 	dframe$Y.score = as.numeric(data[, 3])
 
 	dframe$true = rep('spurious', length(ordering))
-	dframe$consensus = rep('', length(ordering))
+	dframe$model = rep('', length(ordering))
 
 	# edges as points with special mark
 	for(i in 1:length(ordering))
@@ -34,8 +34,15 @@ guido.plot = function(true.model, consensus.model, X.scores, Y.scores, labels = 
 		if(true.model[ordering[i]] == 1)
 			dframe[i, 'true'] = 'true'
 		
-		if(consensus.model[ordering[i]] == 1)
-			dframe[i, 'consensus'] = 'C'
+		if(consensus.model[ordering[i]] == 1 && standard.model[ordering[i]] == 1)
+			dframe[i, 'model'] = '*'
+			
+		if(consensus.model[ordering[i]] == 1 && standard.model[ordering[i]] == 0)
+			dframe[i, 'model'] = 'C'
+
+		if(consensus.model[ordering[i]] == 0 && standard.model[ordering[i]] == 1)
+			dframe[i, 'model'] = 'S'
+					
 	}
 				
 	library(ggplot2)
@@ -44,15 +51,15 @@ guido.plot = function(true.model, consensus.model, X.scores, Y.scores, labels = 
     mycolours = c("true" = "brown3", "spurious" = "black")
     myshape = c("yes" = "a", "no" = "b")
 
-	top.frame = dframe[, c('X', 'X.score', 'true', 'consensus')]
-	mid.frame = dframe[, c('X', 'Y.score', 'true', 'consensus')]
+	top.frame = dframe[, c('X', 'X.score', 'true', 'model')]
+	mid.frame = dframe[, c('X', 'Y.score', 'true', 'model')]
 	
 	p.top = ggplot(top.frame, aes(x = X, y = X.score, fill = labels[1])) +
 	ggtitle(paste(labels[1],'score'))+
-	xlab(paste('X-axis ordered according to the ', labels[1],'score')) +guides(fill=FALSE) +
+	xlab(paste('X-axis ordered according to the ', labels[1],'score (*: both, C: consensus, S: standard)')) +guides(fill=FALSE) +
     geom_point(size = 2,  aes(colour = true)) +
     scale_color_manual("Edge", values = mycolours) +
-    geom_text(data = top.frame, size = 3, aes(x = X + 0.05, y = X.score + 5, label = top.frame$consensus)) 
+    geom_text(data = top.frame, size = 3, aes(x = X + 0.05, y = X.score + 5, label = top.frame$model)) 
 
     # scale_color_manual("Consensus", values = myshape) 
      
@@ -65,7 +72,7 @@ guido.plot = function(true.model, consensus.model, X.scores, Y.scores, labels = 
 
 
 	p.mid = ggplot(mid.frame, aes(x = X, y = Y.score, fill = labels[2])) +
-	xlab(paste('X-axis ordered as in the top panel')) +
+	xlab(paste('X-axis ordered as in the top panel (', labels[1], 'annotated)')) +
 	guides(fill=FALSE) +
 	ggtitle(paste(labels[2],'score'))+
     geom_point(size = 2, aes(colour = true)) +
